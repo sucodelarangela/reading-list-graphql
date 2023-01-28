612f241362c8c4abf3ea1af444fb5b5edcef8107
Angela Caldas <aes.caldas@gmail.com>
2023-01-28 12:33:01 -0300

    feat: set two-way type relations between AuthorType and BookType

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,13 +5,23 @@ const graphql = require('graphql');
 const _ = require('lodash'); // lodash is a utility lib to deal with arrays, objects, etc in JS
 
 // defining object types
-const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema, GraphQLInt } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLList
+} = graphql;
 
 // dummy data for books
 const books = [
   { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
   { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+  { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
 
 // dummy data for authors
@@ -42,7 +52,14 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType), // not ony a BookType, but a list of BookTypes (for we have many books)
+      resolve(parent, args) {
+        // relate the author to a list of books
+        return _.filter(books, { authorId: parent.id });
+      }
+    }
   })
 });
 
